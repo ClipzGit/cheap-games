4300c016e8daba13ffd801d50ac1ec7f8a680851
ClipzGit <cameron-straw@outlook.com>
2022-06-21 20:19:53 +0100

    Entire Request / Theme Update

---
+++ table.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body class="bg-slate-900 ml-11">
+    <table class="table-auto bg-slate-700 rounded-md">
+        <thead class="bg-slate-800 text-white">
+          <tr>
+            <th class="p-2">NAME</th>
+            <th class="p-2">LOWEST PRICE</th>
+            <th class="p-2">ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="uppercase">
+            <td class="p-2 bg-red-400 text-red-900 font-bold">GAME NAME</td>
+            <td class="p-2 bg-green-400 text-green-900 font-bold">£10</td>
+            <td class="p-2 bg-orange-400 text-orange-900 font-bold text-center">VISIT</td>
+          </tr>
+        </tbody>
+      </table>
+</body>
+</html>
